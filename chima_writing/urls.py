6dbd7fbcabcc68560649af82fa0d780cbd1db457
ChijiokeDivine <chijiokedivine10256@gmail.com>
2025-01-08 05:30:57 -0800

    ..

--- chima_writing/urls.py
+++ chima_writing/urls.py
@@ -7,6 +7,7 @@ from chima_writing.sitemaps import StaticSitemap
 from django.conf import settings
 from django.views.static import serve
 from django.urls import re_path
+from django.conf.urls.static import static
 
 
 sitemaps = {
